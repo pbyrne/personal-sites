218da7bc398e5fd11e7577a38f616bb5ef3c874c
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-03-22 13:41:20 -0500

    Add S3 bucket for Battlemap staging

--- battlemap.tf
+++ battlemap.tf
@@ -4,3 +4,20 @@ resource "dnsimple_record" "battlemap-staging" {
   value = "curved-crane-4ddl52l52gdcs3rdi973mnsr.herokudns.com"
   type = "CNAME"
 }
+
+resource "aws_s3_bucket" "battlemap-staging" {
+  bucket = "battlemap-staging"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://staging.battlemap.app"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  versioning {
+    enabled = true
+  }
+}
