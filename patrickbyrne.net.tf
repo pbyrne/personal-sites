d0bd64a79b3eb7c46d1b686b12a2fa429b2d75fc
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-06-10 20:30:15 -0500

    Remove S3 versions of personal site
    Because:
    
    *
    
    Solution:
    
    *

--- patrickbyrne.net.tf
+++ patrickbyrne.net.tf
@@ -1,21 +1,5 @@
 resource "netlify_deploy_key" "patrickbyrne-net" {}
 
-module "patrickbyrne-net" {
-  source = "./modules/s3-static-site/"
-
-  apex      = "patrickbyrne.net"
-  subdomain = "legacy"
-  name      = "patrickbyrne-net"
-}
-
-module "staging-patrickbyrne-net" {
-  source = "./modules/s3-static-site/"
-
-  apex      = "patrickbyrne.net"
-  subdomain = "staging-s3"
-  name      = "staging-patrickbyrne-net"
-}
-
 module "netlify-staging-patrickbyrne-net" {
   source = "./modules/netlify-static-site/"
 
