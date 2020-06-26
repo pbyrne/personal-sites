a4c339e3de17f9b490bbee68d3107d1325407e9d
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-06-26 07:34:35 -0500

    Verify ownership with Google Webmaster Tools

--- dannahforcitycouncil.com.tf
+++ dannahforcitycouncil.com.tf
@@ -11,6 +11,13 @@ module "dannahforcitycouncil-com" {
   deploy_key = netlify_deploy_key.dannahforcitycouncil-com
 }
 
+resource "dnsimple_record" "pjb-google-webmaster-verification" {
+  domain = "dannahforcitycouncil.com"
+  name = ""
+  type = "TXT"
+  value = "google-site-verification=1sCj4T0TDHH437g4zN4nf5PgoSYElxrkfwm-T6ZNJIg"
+}
+
 module "staging-dannahforcitycouncil-com" {
   source = "./modules/netlify-static-site"
 
