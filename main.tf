7c133aaf0807cba6525c596aa95bea77be766854
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-10-13 15:24:29 -0500

    Delete S3 Terraform state
    Because:
    
    * We've transitioned to Terraform Cloud
    
    Solution:
    
    * Remove the now-unnecessary bucket

--- main.tf
+++ main.tf
@@ -24,17 +24,3 @@ terraform {
     }
   }
 }
-
-# make the place to store shared Terraform state
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "patrick-byrne-terraform-state"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
