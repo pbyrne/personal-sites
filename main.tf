45d93cffec3d383066d18d0ae1f40d3e0868adf1
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-10-13 15:26:37 -0500

    Revert "Delete S3 Terraform state"
    This reverts commit 7c133aaf0807cba6525c596aa95bea77be766854.

--- main.tf
+++ main.tf
@@ -24,3 +24,17 @@ terraform {
     }
   }
 }
+
+# make the place to store shared Terraform state
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "patrick-byrne-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
